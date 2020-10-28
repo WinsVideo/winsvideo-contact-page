391709676eb3958beac235308398afa5d70f60d8
Win <49825651+WinsDominoes@users.noreply.github.com>
2020-10-28 17:54:22 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,25 @@
+<?php
+	include_once(" "); //header file
+?>
+	<div class="column">
+		<h1>Contact Us!</h1>
+			<hr>
+					<b>Contact Information</b>
+					<p>Want to contact us? Here are the ways to contact WinsVideo
 
+						<br><b>- 1. Email:</b>
+
+							<br>Our General Email: contact@winsvideo.net
+							<br>To report a DMCA request: dmca@winsvideo.net
+							<br>To contact to the administration: admin@winsvideo.net
+
+						<br><b>- 2. Discord</b>
+							<br>Join our discord server for more discussion!
+								<br><a href="https://discord.gg/dZyTKxp">https://discord.gg/dZyTKxp</a></p>
+						<b>- 3. Twitter</b>
+							<br>Tweet to us! Make a tweet, then @ us with the username @WinsVideo :D
+								<br><a href="https://twitter.com/WinsVideo">https://twitter.com/WinsVideo</a>
+	</div>
+<?php
+	include_once(" "); // footer file
+?>
